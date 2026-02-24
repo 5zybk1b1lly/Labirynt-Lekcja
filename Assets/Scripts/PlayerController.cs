6756fd1cdb79112b60b4158c07ebeef642dd14b7
5zybk1b1lly <paluchszymon236@gmail.com>
2026-02-24 17:57:47 +0100

    Ulepszenie skryptu PlayerController oraz stworzenie prefabów

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,6 +27,28 @@ public class PlayerController : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundCheck.position, Vector3.down, out hit, .4f, groundMask))
+        {
+            string terrainType = hit.collider.gameObject.tag;
+
+            switch (terrainType)
+            {
+                case "Standard":
+                    speed = 12f;
+                    break;
+                case "Low":
+                    speed = 3f;
+                    break;
+                case "High":
+                    speed = 20f;
+                    break;
+                default:
+                    speed = 12f;
+                    break;
+            }
+        }
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
