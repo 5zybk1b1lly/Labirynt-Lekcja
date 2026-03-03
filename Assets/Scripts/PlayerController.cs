083af6102dd998eebd2931aafd9dc6605fc669dd
5zybk1b1lly <paluchszymon236@gmail.com>
2026-03-03 18:14:42 +0100

    Stworzenie prototypu PickUp'u oraz skryptu dla niego
    Utworzenie protypów dla kolejnych PickUp'ów

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,4 +53,11 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
     }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "PickUp")
+        {
+            hit.gameObject.GetComponent<PickUp>().Picked();
+        }
+    }
 }
