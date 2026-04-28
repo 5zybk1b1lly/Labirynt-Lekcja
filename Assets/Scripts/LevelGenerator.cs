cae73be769ddb23c4ae8a03e405eafb96aa75d48
5zybk1b1lly <paluchszymon236@gmail.com>
2026-04-28 17:55:48 +0100

    Napisanie skryptu LevelGenerator oraz mapowanie kolorów na prefab

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -18,4 +18,31 @@ public class LevelGenerator : MonoBehaviour
     {
         
     }
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    public void GenerateLabirynth()
+    {
+        for (int x=0; x <map.width; x++)
+        {
+            for (int z=0; z < map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+    }
 }
